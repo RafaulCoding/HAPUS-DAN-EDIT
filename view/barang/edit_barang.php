a234c568d2cd6a6bac8f0a0d5fde11cc3b0e7862
RafaulCoding <rafasukono@gmail.com>
2025-02-25 10:55:03 +0700

    Tugas lanjutan

---
+++ view/barang/edit_barang.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ralzz Store</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Ralzz Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../barang/index.php">Barang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../jenis/index.php">Jenis</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../barang/index.php">Home</a></li>
+                            <li><a class="dropdown-item" href="../barang/index.php">Barang</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="../jenis/index.php">Jenis</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <h2>Edit Data barang</h2>
+        <?php
+        include '../../config/koneksi.php';
+
+        $id_barang = $_GET['id_barang'];
+        $query = mysqli_query($conn, "SELECT * FROM barang WHERE id_barang = '$id_barang'");
+        $result = mysqli_fetch_array($query);
+        ?>
+        <form action="proses_edit_barang.php?id_barang=<?php echo $result['id_barang']; ?>" method="POST">
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">ID Barang</label>
+                <input type="number" class="form-control" name="id_barang" value="<?php echo $result['id_barang']; ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">ID Jenis</label>
+                <input type="number" class="form-control" name="id_jenis" value="<?php echo $result['id_jenis']; ?>" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Nama Barang</label>
+                <input type="text" class="form-control" name="nama_barang" value="<?php echo $result['nama_barang']; ?>" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Harga</label>
+                <input type="number" class="form-control" name="harga" value="<?php echo $result['harga']; ?>" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">stok</label>
+                <input type="number" class="form-control" name="stok" value="<?php echo $result['stok']; ?>" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ view/jenis/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ralzz Store</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Ralzz Store</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../barang/index.php">Barang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../jenis/index.php">Jenis</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="../barang/index.php">Home</a></li>
+                            <li><a class="dropdown-item" href="../barang/index.php">Barang</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="../jenis/index.php">Jenis</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>Data Jenis</h1>
+        <a class="btn btn-primary" href="view_tambah.php" role="button">Tambah Jenis</a>
+        <br><br>
+        <table class="table">
+            <tr>
+                <th scope="col">Id Jenis</th>
+                <th scope="col">Nama Jenis</th>
+                <th scope="col"> Aksi</th>
+            </tr>
+            <?php
+                include '../../Config/Koneksi.php';
+                $query = mysqli_query($conn, "SELECT * FROM jenis");
+                if(mysqli_num_rows($query)) {
+                    while($result = mysqli_fetch_assoc($query)) {
+            ?>
+            <tr>
+                <td><?php echo $result['id_jenis'] ?></td>
+                <td><?php echo $result['nama_jenis'] ?></td>
+                <td>
+                    <a href="edit_jenis.php?id_jenis=<?php echo $result['id_jenis']?>" class="btn-outline-danger">
+                    <button type="submit" class="btn btn-primary">
+                    <i class="fa-solid fa-pen-to-square"></i>Edit</button>
+                </td>
+            </tr>
+            <?php
+                    }
+                } else {
+                    echo "<tr><td colspan='6'>Data Kosong</td></tr>";
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
